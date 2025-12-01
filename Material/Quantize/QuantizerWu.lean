d9e5548d9f0a0001bd6c28f23bbd94d2546ff6a3
Acomage <acomage468@gmail.com>
2025-12-01 16:28:22 +0800

    just save

--- Material/Quantize/QuantizerWu.lean
+++ Material/Quantize/QuantizerWu.lean
@@ -14,6 +14,19 @@ abbrev INDEX_COUNT := 33
 
 abbrev TOTAL_SIZE := 35937
 
+structure Box where
+  r0 : UInt32
+  r1 : UInt32
+  g0 : UInt32
+  g1 : UInt32
+  b0 : UInt32
+  b1 : UInt32
+
+inductive Direction
+  | kRed
+  | kGreen
+  | kBlue
+
 -- TODO: prove this
 
 /--
@@ -24,21 +37,21 @@ def getIndex (r g b : UInt32) : Fin TOTAL_SIZE :=
   have h : n < TOTAL_SIZE := by sorry
   ⟨n, h⟩
 
-def constructHistogram (pixels : Array UInt32) (moments : ST.Ref σ (Vector (UInt32 × UInt32 × UInt32 × UInt32 × Float) TOTAL_SIZE)) : ST σ Unit := do
+def constructHistogram (pixels : Array UInt32) (moments : ST.Ref σ (Vector (Vector Int64 5) TOTAL_SIZE)) : ST σ Unit := do
   for pixel in pixels do
     let rgb := rgbFromArgb pixel
     let bitsToRemove := 8 - INDEX_BITS
     let indexRGB := rgb.map (fun c => (c >>> bitsToRemove) + 1)
     let index := getIndex indexRGB[0] indexRGB[1] indexRGB[2]
-    moments.modify (fun m => m.modify index fun (w, mr, mg, mb, ms) => (w + 1, mr + rgb[0], mg + rgb[1], mb + rgb[2], ms + (rgb[0] * rgb[0] + rgb[1] * rgb[1] + rgb[2] * rgb[2]).toFloat))
+    let rgb64 := rgb.map (·.toNat.toInt64)
+    let rgbSq := (rgb64.map (·^2)).sum
+    moments.modify (fun m => m.modify index (fun x => x + (rgb64.append #v[1, rgbSq])))
 
-
-/- def computeMoments (weights momentsR momentsG momentsB : ST.Ref σ (Vector UInt32 TOTAL_SIZE)) (moments : ST.Ref σ (Vector Float TOTAL_SIZE)): ST σ Unit := sorry -/
-def computeMoments (moments : ST.Ref σ (Vector (UInt32 × UInt32 × UInt32 × UInt32 × Float) TOTAL_SIZE)): ST σ Unit := do
+def computeMoments (moments : ST.Ref σ (Vector (Vector Int64 5) TOTAL_SIZE)): ST σ Unit := do
     for hr : r in [1:INDEX_COUNT] do
-      let mut area : Vector (UInt32 × UInt32 × UInt32 × UInt32 × Float) INDEX_COUNT := Vector.replicate INDEX_COUNT (0, 0, 0, 0, 0.0)
+      let mut area : Vector (Vector Int64 5) INDEX_COUNT := Vector.replicate INDEX_COUNT #v[0, 0, 0, 0, 0]
       for hg : g in [1:INDEX_COUNT] do
-        let mut line : UInt32 × UInt32 × UInt32 × UInt32 × Float := (0, 0, 0, 0, 0.0)
+        let mut line : Vector Int64 5 := #v[0, 0, 0, 0, 0]
         for b in ((Array.finRange (INDEX_COUNT))[1:]) do
           let index := getIndex r.toUInt32 g.toUInt32 b.toNat.toUInt32
           line := line + ((←moments.get)[index])
@@ -46,8 +59,75 @@ def computeMoments (moments : ST.Ref σ (Vector (UInt32 × UInt32 × UInt32 × U
           let prevIndex := getIndex (r-1).toUInt32 g.toUInt32 b.toNat.toUInt32
           moments.modify (fun m => m.modify index (fun x => m[prevIndex] + area[b]))
 
+def top (cube : Box) (direction : Direction) (position : UInt32) (moment : Vector Int64 TOTAL_SIZE) : Int64 :=
+  match direction with
+  | .kRed =>
+    moment[getIndex position cube.g1 cube.b1] -
+    moment[getIndex position cube.g1 cube.b0] -
+    moment[getIndex position cube.g0 cube.b1] +
+    moment[getIndex position cube.g0 cube.b0]
+  | .kGreen =>
+    moment[getIndex cube.r1 position cube.b1] -
+    moment[getIndex cube.r1 position cube.b0] -
+    moment[getIndex cube.r0 position cube.b1] +
+    moment[getIndex cube.r0 position cube.b0]
+  | .kBlue =>
+    moment[getIndex cube.r1 cube.g1 position] -
+    moment[getIndex cube.r1 cube.g0 position] -
+    moment[getIndex cube.r0 cube.g1 position] +
+    moment[getIndex cube.r0 cube.g0 position]
+
+def bottom (cube : Box) (direction : Direction) (moment : Vector Int64 TOTAL_SIZE) : Int64 :=
+  match direction with
+  | .kRed =>
+    -moment[getIndex cube.r0 cube.g1 cube.b1] +
+    moment[getIndex cube.r0 cube.g1 cube.b0] +
+    moment[getIndex cube.r0 cube.g0 cube.b1] -
+    moment[getIndex cube.r0 cube.g0 cube.b0]
+  | .kGreen =>
+    -moment[getIndex cube.r1 cube.g0 cube.b1] +
+    moment[getIndex cube.r1 cube.g0 cube.b0] +
+    moment[getIndex cube.r0 cube.g0 cube.b1] -
+    moment[getIndex cube.r0 cube.g0 cube.b0]
+  | .kBlue =>
+    -moment[getIndex cube.r1 cube.g1 cube.b0] +
+    moment[getIndex cube.r1 cube.g0 cube.b0] +
+    moment[getIndex cube.r0 cube.g1 cube.b0] -
+    moment[getIndex cube.r0 cube.g0 cube.b0]
+
+def vol (cube : Box) (moment : Vector Int64 TOTAL_SIZE) : Int64 :=
+  moment[getIndex cube.r1 cube.g1 cube.b1] -
+  moment[getIndex cube.r1 cube.g1 cube.b0] -
+  moment[getIndex cube.r1 cube.g0 cube.b1] +
+  moment[getIndex cube.r1 cube.g0 cube.b0] -
+  moment[getIndex cube.r0 cube.g1 cube.b1] +
+  moment[getIndex cube.r0 cube.g1 cube.b0] +
+  moment[getIndex cube.r0 cube.g0 cube.b1] -
+  moment[getIndex cube.r0 cube.g0 cube.b0]
+
+def vol' (cube : Box) (moments : Vector (Vector Int64 5) TOTAL_SIZE) : Vector Int64 5 :=
+  moments[getIndex cube.r1 cube.g1 cube.b1] -
+  moments[getIndex cube.r1 cube.g1 cube.b0] -
+  moments[getIndex cube.r1 cube.g0 cube.b1] +
+  moments[getIndex cube.r1 cube.g0 cube.b0] -
+  moments[getIndex cube.r0 cube.g1 cube.b1] +
+  moments[getIndex cube.r0 cube.g1 cube.b0] +
+  moments[getIndex cube.r0 cube.g0 cube.b1] -
+  moments[getIndex cube.r0 cube.g0 cube.b0]
+
+def variance (cube : Box) (moments : Vector (Vector Int64 5) TOTAL_SIZE) : Float :=
+  let d := (vol' cube moments).map Int64.toFloat
+  let xx := d[4]
+  let hypotenuse := d[0] * d[0] + d[1] * d[1] + d[2] * d[2]
+  let volume := d[3]
+  xx - hypotenuse / volume
+
+def maximize (cube : Box) (direction : Direction) (first last : UInt32) (cut :ST.Ref σ UInt32) (whole_w whole_r whole_g whole_b : Int64) (moments : Vector (Vector Int64 5) TOTAL_SIZE) : ST σ Float := sorry
+
+def cut (box1 box2 : Box) (moments : Vector (Vector Int64 5) TOTAL_SIZE) : Bool := sorry
+
 def QuantizerWu (pixels : Array UInt32) (max_colors : UInt8) : Array UInt32 := runST fun s => do
-  let momentsRef : ST.Ref s (Vector (UInt32 × UInt32 × UInt32 × UInt32 × Float) TOTAL_SIZE) ← ST.mkRef (Vector.replicate TOTAL_SIZE (0, 0, 0, 0, 0.0))
+  let momentsRef : ST.Ref s (Vector (Vector Int64 5) TOTAL_SIZE) ← ST.mkRef (Vector.replicate TOTAL_SIZE #v[0, 0, 0, 0, 0])
   constructHistogram pixels momentsRef
   computeMoments momentsRef
   sorry
