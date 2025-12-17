15f1ff41b8c90a5b95ba9ccbf2f8f031d70973fc
Acomage <acomage468@gmail.com>
2025-12-17 16:24:31 +0800

    comment unused code.

--- Material/DynamicColor/DynamicScheme.lean
+++ Material/DynamicColor/DynamicScheme.lean
@@ -23,171 +23,171 @@ def getRotatedHue (sourceColor : Hct) (hues : Vector Float 9) (rotations : Vecto
         return sanitizeDegreesDouble (sourceHue + rotations[i])
   return sourceHue
 
-def sourceColorArgb (ds : DynamicScheme) : UInt32 := ds.sourceColorHct.toInt
+/- def sourceColorArgb (ds : DynamicScheme) : UInt32 := ds.sourceColorHct.toInt -/
 
 open MaterialDynamicColors
 
-def getPrimaryPaletteKeyColor (ds : DynamicScheme) : UInt32 :=
-  primaryPaletteKeyColor.getArgb ds
+/- def getPrimaryPaletteKeyColor (ds : DynamicScheme) : UInt32 := -/
+/-   primaryPaletteKeyColor.getArgb ds -/
 
-def getSecondaryPaletteKeyColor (ds : DynamicScheme) : UInt32 :=
-  secondaryPaletteKeyColor.getArgb ds
+/- def getSecondaryPaletteKeyColor (ds : DynamicScheme) : UInt32 := -/
+/-   secondaryPaletteKeyColor.getArgb ds -/
 
-def getTertiaryPaletteKeyColor (ds : DynamicScheme) : UInt32 :=
-  tertiaryPaletteKeyColor.getArgb ds
+/- def getTertiaryPaletteKeyColor (ds : DynamicScheme) : UInt32 := -/
+/-   tertiaryPaletteKeyColor.getArgb ds -/
 
-def getNeutralPaletteKeyColor (ds : DynamicScheme) : UInt32 :=
-  neutralPaletteKeyColor.getArgb ds
+/- def getNeutralPaletteKeyColor (ds : DynamicScheme) : UInt32 := -/
+/-   neutralPaletteKeyColor.getArgb ds -/
 
-def getNeutralVariantPaletteKeyColor (ds : DynamicScheme) : UInt32 :=
-  neutralVariantPaletteKeyColor.getArgb ds
+/- def getNeutralVariantPaletteKeyColor (ds : DynamicScheme) : UInt32 := -/
+/-   neutralVariantPaletteKeyColor.getArgb ds -/
 
-def getBackground (ds : DynamicScheme) : UInt32 :=
-  background.getArgb ds
+/- def getBackground (ds : DynamicScheme) : UInt32 := -/
+/-   background.getArgb ds -/
 
-def getOnBackground (ds : DynamicScheme) : UInt32 :=
-  onBackground.getArgb ds
+/- def getOnBackground (ds : DynamicScheme) : UInt32 := -/
+/-   onBackground.getArgb ds -/
 
-def getSurface (ds : DynamicScheme) : UInt32 :=
-  surface.getArgb ds
+/- def getSurface (ds : DynamicScheme) : UInt32 := -/
+/-   surface.getArgb ds -/
 
-def getSurfaceDim (ds : DynamicScheme) : UInt32 :=
-  surfaceDim.getArgb ds
+/- def getSurfaceDim (ds : DynamicScheme) : UInt32 := -/
+/-   surfaceDim.getArgb ds -/
 
-def getSurfaceBright (ds : DynamicScheme) : UInt32 :=
-  surfaceBright.getArgb ds
+/- def getSurfaceBright (ds : DynamicScheme) : UInt32 := -/
+/-   surfaceBright.getArgb ds -/
 
-def getSurfaceContainerLowest (ds : DynamicScheme) : UInt32 :=
-  surfaceContainerLowest.getArgb ds
+/- def getSurfaceContainerLowest (ds : DynamicScheme) : UInt32 := -/
+/-   surfaceContainerLowest.getArgb ds -/
 
-def getSurfaceContainerLow (ds : DynamicScheme) : UInt32 :=
-  surfaceContainerLow.getArgb ds
+/- def getSurfaceContainerLow (ds : DynamicScheme) : UInt32 := -/
+/-   surfaceContainerLow.getArgb ds -/
 
-def getSurfaceContainer (ds : DynamicScheme) : UInt32 :=
-  surfaceContainer.getArgb ds
+/- def getSurfaceContainer (ds : DynamicScheme) : UInt32 := -/
+/-   surfaceContainer.getArgb ds -/
 
-def getSurfaceContainerHigh (ds : DynamicScheme) : UInt32 :=
-  surfaceContainerHigh.getArgb ds
+/- def getSurfaceContainerHigh (ds : DynamicScheme) : UInt32 := -/
+/-   surfaceContainerHigh.getArgb ds -/
 
-def getSurfaceContainerHighest (ds : DynamicScheme) : UInt32 :=
-  surfaceContainerHighest.getArgb ds
+/- def getSurfaceContainerHighest (ds : DynamicScheme) : UInt32 := -/
+/-   surfaceContainerHighest.getArgb ds -/
 
-def getOnSurface (ds : DynamicScheme) : UInt32 :=
-  onSurface.getArgb ds
+/- def getOnSurface (ds : DynamicScheme) : UInt32 := -/
+/-   onSurface.getArgb ds -/
 
-def getSurfaceVariant (ds : DynamicScheme) : UInt32 :=
-  surfaceVariant.getArgb ds
+/- def getSurfaceVariant (ds : DynamicScheme) : UInt32 := -/
+/-   surfaceVariant.getArgb ds -/
 
-def getOnSurfaceVariant (ds : DynamicScheme) : UInt32 :=
-  onSurfaceVariant.getArgb ds
+/- def getOnSurfaceVariant (ds : DynamicScheme) : UInt32 := -/
+/-   onSurfaceVariant.getArgb ds -/
 
-def getInverseSurface (ds : DynamicScheme) : UInt32 :=
-  inverseSurface.getArgb ds
+/- def getInverseSurface (ds : DynamicScheme) : UInt32 := -/
+/-   inverseSurface.getArgb ds -/
 
-def getInverseOnSurface (ds : DynamicScheme) : UInt32 :=
-  inverseOnSurface.getArgb ds
+/- def getInverseOnSurface (ds : DynamicScheme) : UInt32 := -/
+/-   inverseOnSurface.getArgb ds -/
 
-def getOutline (ds : DynamicScheme) : UInt32 :=
-  outline.getArgb ds
+/- def getOutline (ds : DynamicScheme) : UInt32 := -/
+/-   outline.getArgb ds -/
 
-def getOutlineVariant (ds : DynamicScheme) : UInt32 :=
-  outlineVariant.getArgb ds
+/- def getOutlineVariant (ds : DynamicScheme) : UInt32 := -/
+/-   outlineVariant.getArgb ds -/
 
-def getShadow (ds : DynamicScheme) : UInt32 :=
-  shadow.getArgb ds
+/- def getShadow (ds : DynamicScheme) : UInt32 := -/
+/-   shadow.getArgb ds -/
 
-def getScrim (ds : DynamicScheme) : UInt32 :=
-  scrim.getArgb ds
+/- def getScrim (ds : DynamicScheme) : UInt32 := -/
+/-   scrim.getArgb ds -/
 
-def getSurfaceTint (ds : DynamicScheme) : UInt32 :=
-  surfaceTint.getArgb ds
+/- def getSurfaceTint (ds : DynamicScheme) : UInt32 := -/
+/-   surfaceTint.getArgb ds -/
 
-def getPrimary (ds : DynamicScheme) : UInt32 :=
-  primary.getArgb ds
+/- def getPrimary (ds : DynamicScheme) : UInt32 := -/
+/-   primary.getArgb ds -/
 
-def getOnPrimary (ds : DynamicScheme) : UInt32 :=
-  onPrimary.getArgb ds
+/- def getOnPrimary (ds : DynamicScheme) : UInt32 := -/
+/-   onPrimary.getArgb ds -/
 
-def getPrimaryContainer (ds : DynamicScheme) : UInt32 :=
-  primaryContainer.getArgb ds
+/- def getPrimaryContainer (ds : DynamicScheme) : UInt32 := -/
+/-   primaryContainer.getArgb ds -/
 
-def getOnPrimaryContainer (ds : DynamicScheme) : UInt32 :=
-  onPrimaryContainer.getArgb ds
+/- def getOnPrimaryContainer (ds : DynamicScheme) : UInt32 := -/
+/-   onPrimaryContainer.getArgb ds -/
 
-def getInversePrimary (ds : DynamicScheme) : UInt32 :=
-  inversePrimary.getArgb ds
+/- def getInversePrimary (ds : DynamicScheme) : UInt32 := -/
+/-   inversePrimary.getArgb ds -/
 
-def getSecondary (ds : DynamicScheme) : UInt32 :=
-  secondary.getArgb ds
+/- def getSecondary (ds : DynamicScheme) : UInt32 := -/
+/-   secondary.getArgb ds -/
 
-def getOnSecondary (ds : DynamicScheme) : UInt32 :=
-  onSecondary.getArgb ds
+/- def getOnSecondary (ds : DynamicScheme) : UInt32 := -/
+/-   onSecondary.getArgb ds -/
 
-def getSecondaryContainer (ds : DynamicScheme) : UInt32 :=
-  secondaryContainer.getArgb ds
+/- def getSecondaryContainer (ds : DynamicScheme) : UInt32 := -/
+/-   secondaryContainer.getArgb ds -/
 
-def getOnSecondaryContainer (ds : DynamicScheme) : UInt32 :=
-  onSecondaryContainer.getArgb ds
+/- def getOnSecondaryContainer (ds : DynamicScheme) : UInt32 := -/
+/-   onSecondaryContainer.getArgb ds -/
 
-def getTertiary (ds : DynamicScheme) : UInt32 :=
-  tertiary.getArgb ds
+/- def getTertiary (ds : DynamicScheme) : UInt32 := -/
+/-   tertiary.getArgb ds -/
 
-def getOnTertiary (ds : DynamicScheme) : UInt32 :=
-  onTertiary.getArgb ds
+/- def getOnTertiary (ds : DynamicScheme) : UInt32 := -/
+/-   onTertiary.getArgb ds -/
 
-def getTertiaryContainer (ds : DynamicScheme) : UInt32 :=
-  tertiaryContainer.getArgb ds
+/- def getTertiaryContainer (ds : DynamicScheme) : UInt32 := -/
+/-   tertiaryContainer.getArgb ds -/
 
-def getOnTertiaryContainer (ds : DynamicScheme) : UInt32 :=
-  onTertiaryContainer.getArgb ds
+/- def getOnTertiaryContainer (ds : DynamicScheme) : UInt32 := -/
+/-   onTertiaryContainer.getArgb ds -/
 
-def getError (ds : DynamicScheme) : UInt32 :=
-  error.getArgb ds
+/- def getError (ds : DynamicScheme) : UInt32 := -/
+/-   error.getArgb ds -/
 
-def getOnError (ds : DynamicScheme) : UInt32 :=
-  onError.getArgb ds
+/- def getOnError (ds : DynamicScheme) : UInt32 := -/
+/-   onError.getArgb ds -/
 
-def getErrorContainer (ds : DynamicScheme) : UInt32 :=
-  errorContainer.getArgb ds
+/- def getErrorContainer (ds : DynamicScheme) : UInt32 := -/
+/-   errorContainer.getArgb ds -/
 
-def getOnErrorContainer (ds : DynamicScheme) : UInt32 :=
-  onErrorContainer.getArgb ds
+/- def getOnErrorContainer (ds : DynamicScheme) : UInt32 := -/
+/-   onErrorContainer.getArgb ds -/
 
-def getPrimaryFixed (ds : DynamicScheme) : UInt32 :=
-  primaryFixed.getArgb ds
+/- def getPrimaryFixed (ds : DynamicScheme) : UInt32 := -/
+/-   primaryFixed.getArgb ds -/
 
-def getPrimaryFixedDim (ds : DynamicScheme) : UInt32 :=
-  primaryFixedDim.getArgb ds
+/- def getPrimaryFixedDim (ds : DynamicScheme) : UInt32 := -/
+/-   primaryFixedDim.getArgb ds -/
 
-def getOnPrimaryFixed (ds : DynamicScheme) : UInt32 :=
-  onPrimaryFixed.getArgb ds
+/- def getOnPrimaryFixed (ds : DynamicScheme) : UInt32 := -/
+/-   onPrimaryFixed.getArgb ds -/
 
-def getOnPrimaryFixedVariant (ds : DynamicScheme) : UInt32 :=
-  onPrimaryFixedVariant.getArgb ds
+/- def getOnPrimaryFixedVariant (ds : DynamicScheme) : UInt32 := -/
+/-   onPrimaryFixedVariant.getArgb ds -/
 
-def getSecondaryFixed (ds : DynamicScheme) : UInt32 :=
-  secondaryFixed.getArgb ds
+/- def getSecondaryFixed (ds : DynamicScheme) : UInt32 := -/
+/-   secondaryFixed.getArgb ds -/
 
-def getSecondaryFixedDim (ds : DynamicScheme) : UInt32 :=
-  secondaryFixedDim.getArgb ds
+/- def getSecondaryFixedDim (ds : DynamicScheme) : UInt32 := -/
+/-   secondaryFixedDim.getArgb ds -/
 
-def getOnSecondaryFixed (ds : DynamicScheme) : UInt32 :=
-  onSecondaryFixed.getArgb ds
+/- def getOnSecondaryFixed (ds : DynamicScheme) : UInt32 := -/
+/-   onSecondaryFixed.getArgb ds -/
 
-def getOnSecondaryFixedVariant (ds : DynamicScheme) : UInt32 :=
-  onSecondaryFixedVariant.getArgb ds
+/- def getOnSecondaryFixedVariant (ds : DynamicScheme) : UInt32 := -/
+/-   onSecondaryFixedVariant.getArgb ds -/
 
-def getTertiaryFixed (ds : DynamicScheme) : UInt32 :=
-  tertiaryFixed.getArgb ds
+/- def getTertiaryFixed (ds : DynamicScheme) : UInt32 := -/
+/-   tertiaryFixed.getArgb ds -/
 
-def getTertiaryFixedDim (ds : DynamicScheme) : UInt32 :=
-  tertiaryFixedDim.getArgb ds
+/- def getTertiaryFixedDim (ds : DynamicScheme) : UInt32 := -/
+/-   tertiaryFixedDim.getArgb ds -/
 
-def getOnTertiaryFixed (ds : DynamicScheme) : UInt32 :=
-  onTertiaryFixed.getArgb ds
+/- def getOnTertiaryFixed (ds : DynamicScheme) : UInt32 := -/
+/-   onTertiaryFixed.getArgb ds -/
 
-def getOnTertiaryFixedVariant (ds : DynamicScheme) : UInt32 :=
-  onTertiaryFixedVariant.getArgb ds
+/- def getOnTertiaryFixedVariant (ds : DynamicScheme) : UInt32 := -/
+/-   onTertiaryFixedVariant.getArgb ds -/
 
 def showAllColors (ds : DynamicScheme) : String :=
   let entries := [
